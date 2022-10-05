f021fc6317471bfde386b92085747f2c40e80ef7
Dominik Mrówka <dmrowka7@gmail.com>
2022-10-05 19:19:07 +0200

    Add tag entity

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsDataService } from './products-data.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [ProductsController],
